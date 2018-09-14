484251103d2aaae00dc67a8f7b5796d3cdb8db99
Kate <kaygray521@gmail.com>
2018-09-14 17:12:51 -0500

    added display search function

--- script.js
+++ script.js
@@ -14,29 +14,52 @@ $(document).ready(function() {
     console.log(bookApiSearch);
 
     $.ajax({
-          url: bookApiSearch,
-          dataType:'json',
-          type: 'GET',
-          data: "jsonp",
-          success:function(response){
-            console.log(response);
-            var bookTitle = response.items[0].volumeInfo.title;
-            var bookAuthor = response.items[0].volumeInfo.authors;
-              // document.getElementById("outputdiv").innerHTML = response.items.volumeInfo.title ;
-              document.getElementById("outputdivTitle").innerHTML = "Title: " +  bookTitle ;
-                document.getElementById("outputdivAuthor").innerHTML = " Author: " + bookAuthor;
+      url: bookApiSearch,
+      dataType:'json',
+      type: 'GET',
+      data: "jsonp",
+      success:function(response){
+        console.log(response);
+        var bookTitle = response.items[0].volumeInfo.title;
+        var bookAuthor = response.items[0].volumeInfo.authors;
+
+        var bookSrc = "https://via.placeholder.com/350x150";
+
+        // $('#outputdivTitle').html(bookTitle);
+        // $('#outputdivAuthor').html(bookAuthor);
 
 
-                  }
-              });
+      function addDiv(author, bookTitle, bookImg) {
+        var img = document.createElement("IMG");
+        // var src = c;
+
+        var somethingDiv = $( "<div/>", {
+          class: "bookTileClass",
+          text: "Title: " + bookTitle + " Author: "  + author ,
+          attr: src = bookImg,
+        });
+        var bookImgDiv = $( "<img/>", {
+          src : "https://via.placeholder.com/350x150",
+        });
+
+        // $(bookImgDiv).appendTo( ".booksSearchDisplay" );
+        $(bookImgDiv).prependTo( somethingDiv);
+        $(somethingDiv).appendTo( ".booksSearchDisplay" );
+    };
+
+    addDiv(bookAuthor, bookTitle, bookSrc);
 
 
 
 
-      });
+
+      }
+        });
 
 
 
+
+    });
 
 
 
