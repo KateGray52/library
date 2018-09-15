359972d3475566cf75b37b812707dbc282a78789
Kate <kaygray521@gmail.com>
2018-09-15 04:28:54 -0500

    added table for display

--- script.js
+++ script.js
@@ -3,90 +3,102 @@ console.log("connected");
 $(document).ready(function() {
 
 
+      $("#buttonForSearch").click(function () {
+       var searchInput  = $("#searchBooks").val();
+       var numberResponse = "6";
 
+       var bookApiSearch ="https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&maxResults=" + numberResponse;
+       sendRquest(bookApiSearch)
 
+       });
 
-  $("#buttonForSearch").click(function () {
-    var searchInput  = $("#searchBooks").val();
-    var numberResponse = "6";
-
-
-    var bookApiSearch ="https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&maxResults=" + numberResponse;
-    console.log(bookApiSearch);
-
-    $.ajax({
-      url: bookApiSearch,
-      dataType:'json',
-      type: 'GET',
-      data: "jsonp",
-      data: {limit: 3, order: 'desc'},
-      success:function(response){
-        console.log(response);
+    function sendRquest(bookApiSearch){
+      $.ajax({
+        url: bookApiSearch,
+        dataType:'json',
+        type: 'GET',
+        data: "jsonp",
+        success:function(response){
         var responsetext = response.items;
 
-        // var bookTitle = response.items[0].volumeInfo.title;
-        // var bookAuthor = response.items[0].volumeInfo.authors;
-        // var bookSrc = response.items[0].volumeInfo.imageLinks.smallThumbnail;
-
-        $.each(responsetext, function (index, value) {
-          var bookTitle = responsetext[index].volumeInfo.title;
-          var bookAuthor = responsetext[index].volumeInfo.authors;
-          var bookSrc = responsetext[index].volumeInfo.imageLinks.smallThumbnail;
-          console.log(bookTitle, bookAuthor);
-
-          function addDiv(author, bookTitle, bookImg) {
-            var somethingDiv = $( "<div/>", {
-              class: "bookTileClass",
-              text: "Title: " + bookTitle  + " Author: "  + author ,
+          $.each(responsetext, function (index, value) {
+            var bookTitle = responsetext[index].volumeInfo.title;
+            var bookAuthor = responsetext[index].volumeInfo.authors;
+            var bookSrc = responsetext[index].volumeInfo.imageLinks.smallThumbnail;
+          addDiv(bookAuthor, bookTitle, bookSrc, index);
+          //end of response loop
             });
-            var bookImgDiv = $( "<img/>", {
-              src : bookImg,
-            });
-            // $(bookImgDiv).appendTo( ".booksSearchDisplay" );
-            $(bookImgDiv).prependTo( somethingDiv);
-            $(somethingDiv).appendTo( ".booksSearchDisplay" );
-        };
-
-        addDiv(bookAuthor, bookTitle, bookSrc);
-
-
-
-
-
+        //end of response
+        }
+        //end of ajax
         });
+      };
+
+
+    function addDiv(author, bookTitle, bookImg, index) {
+      // var somethingDiv = $( "<div/>", {
+      //   class: "bookTitleClass",
+      //   id: 'book'+index,
+      // });
+
+      var tableRef = document.getElementById('tableId').getElementsByTagName('tbody')[0];
+  // Insert a row in the table at the last row
+  var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+  // Insert a cell in the row at index 0
+  var newImgRow  = newRow.insertCell(0);
+  var newTitleRow  = newRow.insertCell(1);
+  var newAuthorRow  = newRow.insertCell(2);
+  var newBtnRow  = newRow.insertCell(3);
+
+
+  var newSelectBtn = document.createElement("BUTTON");
+  newSelectBtn.addEventListener("click", function() {
+  console.log("clicked");
+  document.getElementById('tableId').deleteRow(this.parentNode.parentNode.rowIndex)
+  });
+  var newselect = document.createTextNode("select");
+  newSelectBtn.appendChild(newselect);
+  document.body.appendChild(newSelectBtn);
+
+  // var imDiv =
+  // var img = $('<img id="dynamic">');
+  // img.attr('src', bookImg);
+  // img.appendTo('#imDiv');
+
+
+
+  newImgRow.appendChild(document.createTextNode("img"));
+  newTitleRow.appendChild(document.createTextNode(bookTitle));
+  newAuthorRow.appendChild(document.createTextNode(author));
+  newBtnRow.appendChild(newSelectBtn);
 
 
 
 
-    //   function addDiv(author, bookTitle, bookImg) {
-    //     var somethingDiv = $( "<div/>", {
-    //       class: "bookTileClass",
-    //       text: "Title: " + bookTitle  + " Author: "  + author ,
-    //     });
-    //     var bookImgDiv = $( "<img/>", {
-    //       src : bookImg,
-    //     });
-    //     // $(bookImgDiv).appendTo( ".booksSearchDisplay" );
-    //     $(bookImgDiv).prependTo( somethingDiv);
-    //     $(somethingDiv).appendTo( ".booksSearchDisplay" );
-    // };
-    //
-    // addDiv(bookAuthor, bookTitle, bookSrc);
+
+
+      // $(ptext).appendTo( somethingDiv);
+      // $(bookImgDiv).prependTo( somethingDiv);
+      // $(somethingDiv).appendTo( ".booksSearchDisplay" );
+      // $(selectBtn).appendTo(somethingDiv);
+
+  };
+
+  // button.addEventListener ("click", function() {
+  // console.log("did something");
+  // });
+
+  // function newButtonClickListener() {
+  //
+  //     console.log("Hello World");
+  //   };
+
+   //  $( ".listSelect" ).click(function() {
+   // console.log("Hello World");
+   // });
 
 
 
-
-
-      }
-        });
-
-
-
-
-    });
-
-
-
-
-
+//end of document ready
 });
