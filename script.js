6ab78a5db2099208c6c83f2fb9f5ba4cf83c0881
Kate <kaygray521@gmail.com>
2018-09-15 14:32:00 -0500

    create and append img nodes

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ console.log("connected");
 $(document).ready(function() {
 
 
-      $("#buttonForSearch").click(function () {
-       var searchInput  = $("#searchBooks").val();
-       var numberResponse = "6";
-
-       var bookApiSearch ="https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&maxResults=" + numberResponse;
-       sendRquest(bookApiSearch)
-
-       });
+  $("#buttonForSearch").click(function () {
+    var searchInput  = $("#searchBooks").val();
+    var numberResponse = "6";
+    var bookApiSearch ="https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&maxResults=" + numberResponse;
+    sendRquest(bookApiSearch)
+    });
 
     function sendRquest(bookApiSearch){
       $.ajax({
@@ -35,70 +33,44 @@ $(document).ready(function() {
       };
 
 
+
     function addDiv(author, bookTitle, bookImg, index) {
-      // var somethingDiv = $( "<div/>", {
-      //   class: "bookTitleClass",
-      //   id: 'book'+index,
-      // });
 
-      var tableRef = document.getElementById('tableId').getElementsByTagName('tbody')[0];
+    var tableRef = document.getElementById('tableId').getElementsByTagName('tbody')[0];
   // Insert a row in the table at the last row
-  var newRow   = tableRef.insertRow(tableRef.rows.length);
-
+    var newRow   = tableRef.insertRow(tableRef.rows.length);
   // Insert a cell in the row at index 0
-  var newImgRow  = newRow.insertCell(0);
-  var newTitleRow  = newRow.insertCell(1);
-  var newAuthorRow  = newRow.insertCell(2);
-  var newBtnRow  = newRow.insertCell(3);
+    var newImgRow  = newRow.insertCell(0);
+    var newTitleRow  = newRow.insertCell(1);
+    var newAuthorRow  = newRow.insertCell(2);
+    var newBtnRow  = newRow.insertCell(3);
+    //make and set button for table
+    var newSelectBtn = document.createElement("BUTTON");
 
+    newSelectBtn.addEventListener("click", function() {
+    console.log("clicked");
+    document.getElementById('tableId').deleteRow(this.parentNode.parentNode.rowIndex)
+    });
+    var newselect = document.createTextNode("select");
+    newSelectBtn.appendChild(newselect);
+    document.body.appendChild(newSelectBtn);
 
-  var newSelectBtn = document.createElement("BUTTON");
-  newSelectBtn.addEventListener("click", function() {
-  console.log("clicked");
-  document.getElementById('tableId').deleteRow(this.parentNode.parentNode.rowIndex)
-  });
-  var newselect = document.createTextNode("select");
-  newSelectBtn.appendChild(newselect);
-  document.body.appendChild(newSelectBtn);
+  //make image for table list
+    var imgForList = document.createElement("IMG");
+    imgForList.setAttribute('src', bookImg);
+    imgForList.setAttribute('id', 'imageId' + index);
+    var imDiv = document.createTextNode("img");
+    imgForList.appendChild(imDiv);
+    document.body.appendChild(imgForList);
 
-  // var imDiv =
-  // var img = $('<img id="dynamic">');
-  // img.attr('src', bookImg);
-  // img.appendTo('#imDiv');
-
-
-
-  newImgRow.appendChild(document.createTextNode("img"));
-  newTitleRow.appendChild(document.createTextNode(bookTitle));
-  newAuthorRow.appendChild(document.createTextNode(author));
-  newBtnRow.appendChild(newSelectBtn);
-
-
-
-
-
-
-      // $(ptext).appendTo( somethingDiv);
-      // $(bookImgDiv).prependTo( somethingDiv);
-      // $(somethingDiv).appendTo( ".booksSearchDisplay" );
-      // $(selectBtn).appendTo(somethingDiv);
+    //append elements to table
+    newImgRow.appendChild(imgForList);
+    newTitleRow.appendChild(document.createTextNode(bookTitle));
+    newAuthorRow.appendChild(document.createTextNode(author));
+    newBtnRow.appendChild(newSelectBtn);
 
   };
 
-  // button.addEventListener ("click", function() {
-  // console.log("did something");
-  // });
-
-  // function newButtonClickListener() {
-  //
-  //     console.log("Hello World");
-  //   };
-
-   //  $( ".listSelect" ).click(function() {
-   // console.log("Hello World");
-   // });
-
-
-
 //end of document ready
 });
+ld(img);
