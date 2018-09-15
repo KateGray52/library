a3d4ed66139a04fbde9eeae47cc837c2d481d845
Kate <kaygray521@gmail.com>
2018-09-14 19:14:39 -0500

    function to display all search results

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ $(document).ready(function() {
 
   $("#buttonForSearch").click(function () {
     var searchInput  = $("#searchBooks").val();
+    var numberResponse = "6";
 
 
-    var bookApiSearch ="https://www.googleapis.com/books/v1/volumes?q=" + searchInput;
+    var bookApiSearch ="https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&maxResults=" + numberResponse;
     console.log(bookApiSearch);
 
     $.ajax({
@@ -18,36 +19,59 @@ $(document).ready(function() {
       dataType:'json',
       type: 'GET',
       data: "jsonp",
+      data: {limit: 3, order: 'desc'},
       success:function(response){
         console.log(response);
-        var bookTitle = response.items[0].volumeInfo.title;
-        var bookAuthor = response.items[0].volumeInfo.authors;
+        var responsetext = response.items;
 
-        var bookSrc = "https://via.placeholder.com/350x150";
+        // var bookTitle = response.items[0].volumeInfo.title;
+        // var bookAuthor = response.items[0].volumeInfo.authors;
+        // var bookSrc = response.items[0].volumeInfo.imageLinks.smallThumbnail;
 
-        // $('#outputdivTitle').html(bookTitle);
-        // $('#outputdivAuthor').html(bookAuthor);
+        $.each(responsetext, function (index, value) {
+          var bookTitle = responsetext[index].volumeInfo.title;
+          var bookAuthor = responsetext[index].volumeInfo.authors;
+          var bookSrc = responsetext[index].volumeInfo.imageLinks.smallThumbnail;
+          console.log(bookTitle, bookAuthor);
+
+          function addDiv(author, bookTitle, bookImg) {
+            var somethingDiv = $( "<div/>", {
+              class: "bookTileClass",
+              text: "Title: " + bookTitle  + " Author: "  + author ,
+            });
+            var bookImgDiv = $( "<img/>", {
+              src : bookImg,
+            });
+            // $(bookImgDiv).appendTo( ".booksSearchDisplay" );
+            $(bookImgDiv).prependTo( somethingDiv);
+            $(somethingDiv).appendTo( ".booksSearchDisplay" );
+        };
+
+        addDiv(bookAuthor, bookTitle, bookSrc);
 
 
-      function addDiv(author, bookTitle, bookImg) {
-        var img = document.createElement("IMG");
-        // var src = c;
 
-        var somethingDiv = $( "<div/>", {
-          class: "bookTileClass",
-          text: "Title: " + bookTitle + " Author: "  + author ,
-          attr: src = bookImg,
+
+
         });
-        var bookImgDiv = $( "<img/>", {
-          src : "https://via.placeholder.com/350x150",
-        });
 
-        // $(bookImgDiv).appendTo( ".booksSearchDisplay" );
-        $(bookImgDiv).prependTo( somethingDiv);
-        $(somethingDiv).appendTo( ".booksSearchDisplay" );
-    };
 
-    addDiv(bookAuthor, bookTitle, bookSrc);
+
+
+    //   function addDiv(author, bookTitle, bookImg) {
+    //     var somethingDiv = $( "<div/>", {
+    //       class: "bookTileClass",
+    //       text: "Title: " + bookTitle  + " Author: "  + author ,
+    //     });
+    //     var bookImgDiv = $( "<img/>", {
+    //       src : bookImg,
+    //     });
+    //     // $(bookImgDiv).appendTo( ".booksSearchDisplay" );
+    //     $(bookImgDiv).prependTo( somethingDiv);
+    //     $(somethingDiv).appendTo( ".booksSearchDisplay" );
+    // };
+    //
+    // addDiv(bookAuthor, bookTitle, bookSrc);
 
 
 
