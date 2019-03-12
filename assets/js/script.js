bb82f3d2285d5f5c6a287f3592bc2c4f00b72be9
Katherine Kote <kaygray521@gmail.com>
2019-03-12 02:21:14 -0500

    No Duplicates,  Alert
    Same book cant be added to bookcase, and will alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,123 +1,159 @@
-$( document ).ready( function () {
+$(document).ready(function() {
 
-	$( document ).on( 'click', '.deleteBtn', function () {
-		var parentDivToRemove = $( this ).closest( "div" );
-		$( this ).closest( "div" ).remove();
-	} );
-
-
-	//click on search input and highlights text
-	$( function () {
-		$( document ).on( 'click', 'input[type=text]', function () {
-			this.select();
-		} );
-	} );
-
-	//search button for sidebar input
-	$( "#buttonForSearch" ).click( function () {
-		$( "#list" ).html( "" );
-		var searchInput = $( "#searchBooks" ).val();
-		var numberResponse = "6";
-		var bookApiSearch = "https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&maxResults=" + numberResponse;
-		sendRquest( bookApiSearch )
-	} );
-
-	//retrieve info from google books api
-	function sendRquest( bookApiSearch ) {
-		$.ajax( {
-			url: bookApiSearch,
-			dataType: 'json',
-			type: 'GET',
-			data: "jsonp",
-			success: function ( response ) {
-				var responsetext = response.items;
-
-				$.each( responsetext, function ( index, value ) {
-					var bookTitle = responsetext[ index ].volumeInfo.title;
-					var bookAuthor = responsetext[ index ].volumeInfo.authors;
-					var bookSrc = responsetext[ index ].volumeInfo.imageLinks.thumbnail;
-					var bookCat = responsetext[ index ].volumeInfo.categories;
-					var bookInfo = responsetext[ index ].volumeInfo.infoLink;
-					addDiv( bookAuthor, bookTitle, bookSrc, index, bookCat, bookInfo );
-					//end of response loop
-				} );
-				//end of response
-			}
-			//end of ajax
-		} );
-	};
-
-    //sidebar search results
-	function addDiv( author, bookTitle, bookImg, index, bookCat, bookInfo ) {
-
-		var infoGroupDiv = $( "<div></div>" )
-			.addClass( "infogroup" );
-
-		var bookpara = $( "<div></div>" )
-			.addClass( "book-name" )
-			.text( bookTitle )
-			.appendTo( infoGroupDiv );
-
-		var authorpara = $( "<div></div>" )
-			.addClass( "author-desc" )
-			.text( author )
-			.appendTo( infoGroupDiv );
-
-		var infolinkpara = $( "<a> </a>" )
-			.attr( "href", bookInfo )
-			.attr("target", "_blank")
-			.text( "Info" )
-			.appendTo( infoGroupDiv );
-
-		var img = $( '<img >' )
-			.attr( 'src', bookImg );
-
-		var imgDiv = $( "<div></div>" );
-		imgDiv.addClass( "img-div" );
-		imgDiv.append( img );
-
-        //create select button to add to bookcase display, with functions for delete and get info
-		var newSelectBtn = $( "<button></button>" )
-			.addClass( "button small" )
-			.text( "Select" )
-			.click( function () {
-				var bookImgMainDiv = document.getElementById( 'bookImgMain' );
-				var imgInDiv = $( this ).parent().siblings( '.img-div' ).children( 'img' );
-				var clonedImg = ( imgInDiv ).clone();
-				var clonedInfo = $( this ).siblings().clone();
-
-				var overlayDiv = $( "<div></div>" )
-					.addClass( "overlay" );
-
-					var newTextDiv = $( "<div></div>" )
-						.addClass( "text" )
-						.append( clonedInfo )
-						.appendTo( overlayDiv );
+  $(document).on('click', '.deleteBtn', function() {
+    var parentDivToRemove = $(this).closest("div");
+    $(this).closest("div").remove();
+  });
 
 
-				var newDivImg = $( "<div></div>" )
-					.addClass( "booksMedia container" )
-					.append( clonedImg )
-					.append( overlayDiv );
+  //click on search input and highlights text
+  $(function() {
+    $(document).on('click', 'input[type=text]', function() {
+      this.select();
+    });
+  });
 
-				var newDeleteIcon = $( "<i> </i>" )
-					.addClass( "fa fa-times deleteBtn" )
-					.appendTo( newDivImg );
+  //search button for sidebar input
+  $("#buttonForSearch").click(function() {
+    $("#list").html("");
+    var searchInput = $("#searchBooks").val();
+    var numberResponse = "6";
+    var bookApiSearch = "https://www.googleapis.com/books/v1/volumes?q=" + searchInput + "&maxResults=" + numberResponse;
+    sendRquest(bookApiSearch)
+  });
 
-				$( bookImgMainDiv ).prepend( newDivImg );
-				$(this).text("added");
-				$(this).addClass("add");
-			} )
-			.appendTo( infoGroupDiv );
+
+  var ids = new Array();
+  var hrefs = new Array();
+  $('.text a').each(function() {
+    // ids.push($(this).attr('id'));
+    hrefs.push($(this).attr('href'));
+  });
+  // console.log(hrefs);
 
 
-		var ListRef = document.getElementById( 'list' );
-		var newDivWrapper = $( "<div></div>" )
-			.addClass( "infowrapper" )
-			.append( imgDiv, infoGroupDiv )
-			.appendTo( ListRef );
+  //retrieve info from google books api
+  function sendRquest(bookApiSearch) {
+    $.ajax({
+      url: bookApiSearch,
+      dataType: 'json',
+      type: 'GET',
+      data: "jsonp",
+      success: function(response) {
+        var responsetext = response.items;
 
-	};
+        $.each(responsetext, function(index, value) {
+          var bookTitle = responsetext[index].volumeInfo.title;
+          var bookAuthor = responsetext[index].volumeInfo.authors;
+          var bookSrc = responsetext[index].volumeInfo.imageLinks.thumbnail;
+          var bookCat = responsetext[index].volumeInfo.categories;
+          var bookInfo = responsetext[index].volumeInfo.infoLink;
+          addDiv(bookAuthor, bookTitle, bookSrc, index, bookCat, bookInfo);
+          //end of response loop
+        });
+        //end of response
+      }
+      //end of ajax
+    });
+  };
 
-	//end of document ready
-} );;
+  //sidebar search results
+  function addDiv(author, bookTitle, bookImg, index, bookCat, bookInfo) {
+
+    var infoGroupDiv = $("<div></div>")
+      .addClass("infogroup");
+
+    var bookpara = $("<div></div>")
+      .addClass("book-name")
+      .text(bookTitle)
+      .appendTo(infoGroupDiv);
+
+    var authorpara = $("<div></div>")
+      .addClass("author-desc")
+      .text(author)
+      .appendTo(infoGroupDiv);
+
+    var infolinkpara = $("<a> </a>")
+      .attr("href", bookInfo)
+      .attr("target", "_blank")
+      .text("Info")
+      .appendTo(infoGroupDiv);
+
+    var img = $('<img >')
+      .attr('src', bookImg);
+
+    var imgDiv = $("<div></div>");
+    imgDiv.addClass("img-div");
+    imgDiv.append(img);
+
+    //create select button to add to bookcase display, with functions for delete and get info
+    var newSelectBtn = $("<button></button>")
+      .addClass("button small")
+      .text("Select")
+      .click(function() {
+
+        var isClicked = $(this).text();
+        var bookImgMainDiv = document.getElementById('bookImgMain');
+
+        var imgInDiv = $(this).parent().siblings('.img-div').children('img');
+        var clonedImg = (imgInDiv).clone();
+        var clonedInfo = $(this).siblings().clone();
+
+        // var ids = new Array();
+
+        var unClonedA = ($(this).siblings("a").attr("href"));
+
+        var overlayDiv = $("<div></div>")
+          .addClass("overlay");
+
+        var newTextDiv = $("<div></div>")
+          .addClass("text")
+          .append(clonedInfo)
+          .appendTo(overlayDiv);
+
+
+        var newDivImg = $("<div></div>")
+          .addClass("booksMedia container")
+          .append(clonedImg)
+          .append(overlayDiv);
+
+        var newDeleteIcon = $("<i> </i>")
+          .addClass("fa fa-times deleteBtn")
+          .appendTo(newDivImg);
+
+
+        $(this).text("Added");
+        $(this).addClass("add");
+
+        // if (isClicked === "Select") {
+        //   $(bookImgMainDiv).prepend(newDivImg);
+        // } else {
+        //   alert("Already Added");
+        // }
+
+        if (hrefs.indexOf(unClonedA) > -1) {
+          console.log("is in the array");
+					alert("Already in your Bookcase");
+        } else {
+          $(bookImgMainDiv).prepend(newDivImg);
+          hrefs.push(unClonedA);
+          console.log("addddd ittt")
+          console.log(hrefs.length);
+        }
+
+      })
+
+
+      .appendTo(infoGroupDiv);
+
+
+    var ListRef = document.getElementById('list');
+    var newDivWrapper = $("<div></div>")
+      .addClass("infowrapper")
+      .append(imgDiv, infoGroupDiv)
+      .appendTo(ListRef);
+
+  };
+
+  //end of document ready
+});;
